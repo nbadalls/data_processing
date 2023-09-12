0c1b01abf93517d5b08ff361b86593ef24901c23
zhangkaixiang <1277046020@qq.com>
2023-09-12 08:56:34 +0800

    在类别初始化的时候执行一次推理获取类别数

--- module/detector/yolo_detector.py
+++ module/detector/yolo_detector.py
@@ -31,6 +31,26 @@ class YoloDet(DetBase):
         self.annotation_dict = annotation_dict
         self.logger.info(f"Yolo Detector init finished\n Model: {model_path} \n Threshold: {thre}")
         self.nms_iou = nms_iou
+        self.class_num = self.get_class_num()
+
+        # 如果self.thre不是list而是单个的数值，则进行扩展复制
+        if isinstance(self.thre, list):
+            self.thre = np.array(self.thre)
+        elif isinstance(self.thre, float):
+            self.logger.info(f"将阈值{self.thre}扩展到{self.class_num}个类别")
+            self.thre = np.ones(self.class_num) * self.thre
+        else:
+            assert type(self.thre) in [list, float], "thre的类型必须是list或者float"
+        assert len(self.thre) == self.class_num, f"阈值的个数与类别数必须相等{len(self.thre)} vs {self.class_num}"
+
+    def get_class_num(self):
+        img = np.random.randn(1, 3, 640, 640).astype("float32")
+        pred = self.session.run([self.session.get_outputs()[0].name],
+                                {self.session.get_inputs()[0].name: img})
+        pred = torch.tensor(pred[0])  # bxnx(nc+5)
+        cls_num = pred.shape[2] - 5
+        self.logger.info(f"检测目标的类别数为{cls_num}")
+        return cls_num
 
     def forward(self, img, img0_shape):
         """
@@ -45,7 +65,7 @@ class YoloDet(DetBase):
                                 {self.session.get_inputs()[0].name: img})
         pred = torch.tensor(pred[0])   # bxnx(nc+5)
         pred = non_max_suppression(pred, 0.1, self.nms_iou, None, False, 1000)
-        thres = np.array(self.thre)
+
         for i, elem in enumerate(pred):
             # 转化回原图
             elem[:, :4] = scale_coords(img.shape[2:], elem[:, :4], img0_shape[i]).round()
@@ -53,7 +73,7 @@ class YoloDet(DetBase):
             # 根据每类的阈值筛选满足条件的检测框
             cls_id = elem[:, 5].astype("int")
             conf = elem[:, 4]
-            batch_thres = thres[cls_id]
+            batch_thres = self.thre[cls_id]
             mask = (conf > batch_thres)
             pred[i] = elem[mask, :]
         return pred
